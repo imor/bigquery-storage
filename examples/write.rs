e38227cce5165fe6d2e6eb75544f7756236df753
Raminder Singh <romi_ssk@yahoo.co.in>
2024-06-19 17:41:44 +0530

    impl auth

--- examples/write.rs
+++ examples/write.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 
-use bigquery_storage::append_rows;
+use bigquery_storage::WriteClient;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -12,6 +12,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
 }
 
 async fn main_impl() -> Result<(), Box<dyn Error>> {
-    append_rows().await?;
+    rustls::crypto::aws_lc_rs::default_provider()
+        .install_default()
+        .expect("Failed to install rustls crypto provider");
+
+    // 2. Load the desired secret (here, a service account key)
+    let sa_key = yup_oauth2::read_service_account_key(
+        "/Users/raminder.singh/Downloads/pg-replicate-test-project-key-2d4fd0720f29.json",
+    )
+    .await?;
+
+    // 3. Create an Authenticator
+    let auth = yup_oauth2::ServiceAccountAuthenticator::builder(sa_key)
+        .build()
+        .await?;
+    let mut client = WriteClient::new(auth).await?;
+    client.append_rows().await?;
+
     Ok(())
 }
--- src/write.rs
+++ src/write.rs
@@ -1,50 +1,91 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, convert::TryInto};
 
-use tonic::transport::{Channel, ClientTlsConfig};
+use hyper_util::client::legacy::connect::Connect;
+use tokio_stream::StreamExt;
+use tonic::{
+    transport::{Channel, ClientTlsConfig},
+    Request,
+};
+use yup_oauth2::authenticator::Authenticator;
 
 use crate::{
     google::cloud::bigquery::storage::v1::{
         append_rows_request::MissingValueInterpretation,
         big_query_write_client::BigQueryWriteClient, AppendRowsRequest,
     },
-    Error, API_DOMAIN, API_ENDPOINT,
+    Error, API_DOMAIN, API_ENDPOINT, API_SCOPE,
 };
 
-async fn new_write_client() -> Result<BigQueryWriteClient<Channel>, Error> {
-    let tls_config = ClientTlsConfig::new().domain_name(API_DOMAIN);
-    let channel = Channel::from_static(API_ENDPOINT)
-        .tls_config(tls_config)?
-        .connect()
-        .await?;
-    let write_client = BigQueryWriteClient::new(channel);
-
-    Ok(write_client)
+pub struct WriteClient<C> {
+    auth: Authenticator<C>,
+    inner: BigQueryWriteClient<Channel>,
 }
 
-pub async fn append_rows() -> Result<(), Error> {
-    let mut client = new_write_client().await?;
+impl<C> WriteClient<C>
+where
+    C: Connect + Clone + Send + Sync + 'static,
+{
+    pub async fn new(auth: Authenticator<C>) -> Result<Self, Error> {
+        let tls_config = ClientTlsConfig::new().domain_name(API_DOMAIN);
+        let channel = Channel::from_static(API_ENDPOINT)
+            .tls_config(tls_config)?
+            .connect()
+            .await?;
+        let inner = BigQueryWriteClient::new(channel);
 
-    let project = "pg-replicate-test-project";
-    let dataset = "test_data_set";
-    let table = "actor";
+        Ok(WriteClient { auth, inner })
+    }
 
-    let write_stream =
-        format!("projects/{project}/datasets/{dataset}/tables/{table}/streams/_default");
+    async fn new_request<D>(&self, t: D) -> Result<Request<D>, Error> {
+        let token = self.auth.token(&[API_SCOPE]).await?;
+        let bearer_token = format!(
+            "Bearer {}",
+            token
+                .token()
+                .ok_or(Error::Auth(yup_oauth2::Error::MissingAccessToken))?
+        );
+        let bearer_value = bearer_token.as_str().try_into()?;
+        let mut req = Request::new(t);
+        let meta = req.metadata_mut();
+        meta.insert("authorization", bearer_value);
+        // meta.insert("x-goog-request-params", params.try_into()?);
+        Ok(req)
+    }
 
-    let trace_id = "test_client".to_string();
+    pub async fn append_rows(&mut self) -> Result<(), Error> {
+        let project = "pg-replicate-test-project";
+        let dataset = "test_data_set";
+        let table = "actor";
 
-    let append_rows_request = AppendRowsRequest {
-        write_stream,
-        offset: None,
-        trace_id,
-        missing_value_interpretations: HashMap::new(),
-        default_missing_value_interpretation: MissingValueInterpretation::Unspecified.into(),
-        rows: None,
-    };
+        let write_stream =
+            format!("projects/{project}/datasets/{dataset}/tables/{table}/streams/_default");
 
-    let _response = client
-        .append_rows(tokio_stream::iter(vec![append_rows_request]))
-        .await?;
+        let trace_id = "test_client".to_string();
 
-    Ok(())
+        let append_rows_request = AppendRowsRequest {
+            write_stream,
+            offset: None,
+            trace_id,
+            missing_value_interpretations: HashMap::new(),
+            default_missing_value_interpretation: MissingValueInterpretation::Unspecified.into(),
+            rows: None,
+        };
+
+        let req = self
+            .new_request(tokio_stream::iter(vec![append_rows_request]))
+            .await?;
+
+        let response = self.inner.append_rows(req).await?;
+
+        println!("streaming response: {response:#?}");
+
+        let mut streaming = response.into_inner();
+
+        while let Some(resp) = streaming.next().await {
+            let resp = resp?;
+            println!("response: {resp:#?}");
+        }
+
+        Ok(())
+    }
 }
